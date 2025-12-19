e4c5eed1990b5267dd5605559925a652684e0984
viceede <d4rell1@yandex.ru>
2025-12-19 04:05:50 +0300

    .

--- src/ChainDemo.java
+++ src/ChainDemo.java
@@ -1,13 +1,11 @@
 public class ChainDemo {
     public static void main(String[] args) {
-        // Создаем обработчики
         Logger debugLogger = new DebugLogger();
         Logger consoleLogger = new ConsoleLogger();
         Logger warningLogger = new WarningLogger();
         Logger errorLogger = new ErrorLogger();
         Logger criticalLogger = new CriticalLogger();
 
-        // Строим цепочку
         debugLogger.setNextLogger(consoleLogger);
         consoleLogger.setNextLogger(warningLogger);
         warningLogger.setNextLogger(errorLogger);
@@ -16,7 +14,6 @@ public class ChainDemo {
         System.out.println("=== Цепочка обработчиков создана ===");
         System.out.println("Порядок: DEBUG -> INFO -> WARNING -> ERROR -> CRITICAL\n");
 
-        // Тестовые сообщения разных уровней
         System.out.println("1. Логирование DEBUG:");
         debugLogger.logMessage(LogLevel.DEBUG, "Это отладочное сообщение");
 
